a9f400128f1b23c00439f56736d8cb48f684c534
manujose2025 <145644012+manujose2025@users.noreply.github.com>
2024-09-07 15:55:26 +0530

    Create facebook_login.java

---
+++ Android/P1/facebook_login.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class facebook_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_facebook_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void openPage(View view) {
+        /*Intent it_fb = new Intent(Intent.ACTION_VIEW);
+        it_fb.setData(Uri.parse("https://www.facebook.com"));
+        startActivity(it_fb);*/
+
+        Intent it_fb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com"));
+        startActivity(it_fb);
+    }
+}
