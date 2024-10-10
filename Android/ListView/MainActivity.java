b0f8557929b35970bb72df2cde44bd8f56e60cf1
manujose2025 <145644012+manujose2025@users.noreply.github.com>
2024-10-10 12:40:15 +0530

    Create MainActivity.java

---
+++ Android/ListView/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.listview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+
+            lv = findViewById(R.id.lv_list);
+            String[] courses = {"MCA", "MBA", "BBA", "BCA"};
+            ArrayAdapter<String> adp = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, courses);
+            lv.setAdapter(adp);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = lv.getItemAtPosition(i).toString();
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+            }
+            });
+
+            return insets;
+        });
+    }
+}
