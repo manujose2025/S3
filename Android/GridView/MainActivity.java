bfa0c3a50a53c0e48d88d73150c34a1b67af4cf3
manujose2025 <145644012+manujose2025@users.noreply.github.com>
2024-10-24 12:44:18 +0530

    Create MainActivity.java

---
+++ Android/GridView/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.gridview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            GridView gv = findViewById(R.id.gv_grid);
+            String[] course = {"MCA", "MBA", "BCA", "BBA"};
+            ArrayAdapter<String> ad = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, course);
+            gv.setAdapter(ad);
+            gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    String item = gv.getItemAtPosition(i).toString();
+                    Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+                }
+            });
+            return insets;
+        });
+    }
+}
